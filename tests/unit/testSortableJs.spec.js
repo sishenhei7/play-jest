3cc1464bbfdb3770c2cab4970dedbe6c0cefec07
sishenhei7 <982505136@qq.com>
2019-08-29 12:59:18 +0800

    chore: test account

--- tests/unit/testSortableJs.spec.js
+++ tests/unit/testSortableJs.spec.js
@@ -4,7 +4,7 @@ jest.mock('sortablejs');
 
 describe('test sortablejs', () => {
   test('sortablejs is mocked', () => {
-    console.log('Sortable', Sortable);
+    // console.log('Sortable', Sortable);
     expect(Sortable._isMockFunction).toBeTruthy();
 
     const sortable = new Sortable(1, 2);
