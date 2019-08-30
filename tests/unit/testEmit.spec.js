791d41f21e43553de5df5b13aa86874a543a846d
sishenhei7 <982505136@qq.com>
2019-08-30 17:06:55 +0800

    fix: fix unexpected token bug

--- tests/unit/testEmit.spec.js
+++ tests/unit/testEmit.spec.js
@@ -1,7 +1,7 @@
 import { shallowMount } from '@vue/test-utils';
 import ChildModule from './helper/ChildModule';
 import ChildComponent from './helper/ChildComponent';
-import ParentComponent from './helper/ParentComponent';
+import ParentComponent from './helper/ParentComponent.vue';
 
 jest.mock('./helper/ChildModule');
 const mockDestroy = jest.fn();
