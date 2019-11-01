7fb20d6f2f76a3c604fcba59f39f38bea27083f4
sishenhei7 <you@example.com>
2019-11-01 10:05:46 +0800

    feat: add flow

--- jest.config.js
+++ jest.config.js
@@ -11,6 +11,15 @@ module.exports = {
     "^.+\\.jsx?$": "babel-jest"
   },
   moduleNameMapper: {
+    // "^vue$": "src/dist/vue.js",
+    "^vue": "<rootDir>/vue/platforms/web/entry-runtime-with-compiler",
+    "^compiler/(.*)$": "<rootDir>/vue/compiler/$1",
+    "^core/(.*)$": "<rootDir>/vue/core/$1",
+    "^shared/(.*)$": "<rootDir>/vue/shared/$1",
+    "^web/(.*)$": "<rootDir>/vue/platforms/web/$1",
+    "^weex/(.*)$": "<rootDir>/vue/platforms/weex/$1",
+    "^server/(.*)$": "<rootDir>/vue/server/$1",
+    "^sfc/(.*)$": "<rootDir>/vue/sfc/$1",
     "^@/(.*)$": "<rootDir>/src/$1"
   },
   snapshotSerializers: [
@@ -20,10 +29,12 @@ module.exports = {
     "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
   ],
   // testURL: "http://localhost/",
-  // collectCoverageFrom: [
-  //   "<rootDir>/src/vuedraggable.js",
-  //   "<rootDir>/src/util/helper.js"
-  // ],
+  collectCoverageFrom: [
+    // "<rootDir>/src/vuedraggable.js",
+    // "<rootDir>/src/util/helper.js",
+    // "src/dist/vue.js",
+    "<rootDir>/vue/**"
+  ],
   transformIgnorePatterns: [
     "node_modules/(?!(babel-jest|jest-vue-preprocessor)/)"
   ],
